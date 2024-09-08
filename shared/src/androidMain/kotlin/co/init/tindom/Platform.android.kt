7bcdad66094e3bfdb160c8643f62a021e03a8f16
M. Gaborik <gaborikm@posam.sk>
2024-09-08 23:08:38 +0200

    reset commit

--- shared/src/androidMain/kotlin/co/init/tindom/Platform.android.kt
+++
@@ -1,7 +0,0 @@
-package co.init.tindom
-
-class AndroidPlatform : Platform {
-    override val name: String = "Android ${android.os.Build.VERSION.SDK_INT}"
-}
-
-actual fun getPlatform(): Platform = AndroidPlatform()
--- shared/src/commonMain/kotlin/co/init/tindom/Platform.kt
+++
@@ -1,7 +0,0 @@
-package co.init.tindom
-
-interface Platform {
-    val name: String
-}
-
-expect fun getPlatform(): Platform
