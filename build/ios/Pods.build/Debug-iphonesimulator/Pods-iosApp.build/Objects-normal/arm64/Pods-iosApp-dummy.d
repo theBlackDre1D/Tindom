532e27ec82f05ac06ff4902f9bea972442f30982
M. Gaborik <michal.gaborik.77@gmail.com>
2024-10-08 16:48:31 +0200

    Init commit

---
+++ build/ios/Pods.build/Debug-iphonesimulator/Pods-iosApp.build/Objects-normal/arm64/Pods-iosApp-dummy.d
@@ -0,0 +1,4 @@
+dependencies: \
+  /Users/seremtinameno/StudioProjects/Tindom/build/ios/Pods.build/Debug-iphonesimulator/Pods-iosApp.build/module.modulemap \
+  /Users/seremtinameno/StudioProjects/Tindom/iosApp/Pods/Target\ Support\ Files/Pods-iosApp/Pods-iosApp-dummy.m \
+  /Applications/Xcode.app/Contents/Developer/Platforms/iPhoneSimulator.platform/Developer/SDKs/iPhoneSimulator.sdk/System/Library/Frameworks/Foundation.framework/Modules/module.modulemap
---
+++ build/ios/Pods.build/Debug-iphonesimulator/Pods-iosApp.build/Objects-normal/arm64/Pods_iosApp_vers.d
@@ -0,0 +1,3 @@
+dependencies: \
+  /Users/seremtinameno/StudioProjects/Tindom/build/ios/Pods.build/Debug-iphonesimulator/Pods-iosApp.build/module.modulemap \
+  /Users/seremtinameno/StudioProjects/Tindom/build/ios/Pods.build/Debug-iphonesimulator/Pods-iosApp.build/DerivedSources/Pods_iosApp_vers.c
